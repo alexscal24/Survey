e258f0a2116cd5384b2b1e6e5d3a4a719e91d382
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-09-08 16:24:08 -0400

    Assignment 3 Fixed

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -6,6 +6,34 @@
 float tempf, tempc, tempk;
 char tempVersion;
 
+/*
+ *Converts the user inputted fahrenheit value into celsius.
+ * Parameters:
+ *      User inputted fahrenheit
+ * Preconditions:
+ *      Inputted value is in fahrenheit.
+ * Return:
+ *      The converted temperature in celsius.
+ */
+float fahrenheitToCelsius(float tempf){
+    tempc = (tempf - CONVERSIONF) / 1.8;
+    return tempc;
+}
+
+/*
+ *Converts the user inputted fahrenheit value into kelvin.
+ * Parameters:
+ *      User inputted fahrenheit value, fahrenheit value converted to celsius.
+ * Preconditions:
+ *      Inputted value is fahrenheit.
+ * Return:
+ *      The converted temperature in kelvin.\
+ */
+float fahrenheitToKelvin(float tempc){
+    tempk = tempc + CONVERSIONK;
+    return tempk;
+}
+
 // Main Start.
 int main(void){
     printf("Enter the temperature in Fahrenheit in the format of ##.## F: \n");
@@ -13,10 +41,11 @@ int main(void){
     printf("You inputted %f %c \n", tempf, tempVersion); // Repeat back to the user what they inputted.
 
     if(tempVersion != 'F'){
+        printf("That is the wrong temperature type.");
         return 1;
     }else{
-        tempc = (tempf - CONVERSIONF) / 1.8; // Convert user inputted Fahrenheit to Celsius.
-        tempk = tempc + CONVERSIONK; // Uses the converted temperature to convert the inputted Fahrenheit to Celsius.
+        tempc = fahrenheitToCelsius(tempf); // Convert user inputted Fahrenheit to Celsius.
+        tempk = fahrenheitToKelvin(tempc); // Uses the converted temperature to convert the inputted Fahrenheit to Celsius.
 
         printf("%f %c converts to: \n"
                "\t %f C \n"
