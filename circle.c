0b9219ff6ed742980a7b036f862b7d928c8131d0
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-09-08 15:53:32 -0400

    Assignment 2 Corrected

--- circle.c
+++ circle.c
@@ -5,6 +5,48 @@
 int radius, boolean;
 float circumference, area, diameter;
 
+/*
+ * Calculate the diameter using the user inputted radius.
+ * Parameters: 
+ *      Radius inputted by user.
+ * Preconditions:
+ *      radius > 0
+ * Return:
+ *      The diameter of the circle.
+ */
+float diameterCalc(int radius){
+    diameter = radius * 2;
+    return diameter;
+}
+
+/*
+ *Calculate the area using the calculated diameter and user inputted radius.
+ * Parameters: 
+ *      Diameter, user inputted radius.
+ * Preconditions:
+ *      Radius > 0
+ * Return:
+ *      The area of the circle.
+ */
+float areaCalc(float diameter, int radius){
+    area = PI * (radius * radius);
+    return area;
+}
+
+/*
+ *Calculate the circumference of the circle using the user inputted radius.
+ * Parameters:
+ *      User inputted radius.
+ * Preconditions:
+ *      radius > 0
+ * Return:
+ *      The circumference of the circle.
+ */
+float circumferenceCalc(int radius){
+    circumference = 2 * PI * radius;
+    return circumference;
+}
+
 // Begin Main.
 int main(void){
     printf("Enter the radius of the circle: \n");
@@ -19,14 +61,17 @@ int main(void){
     }
     
         if(boolean == 0){
-            diameter = radius * 2; // Calculate the diameter.
-            area = PI * (radius * radius); // Calculate the area.
-            circumference = 2 * PI * radius; // Calculate the circumference.
+            diameter = diameterCalc(radius);
+            area = areaCalc(diameter, radius);
+            circumference = circumferenceCalc(radius);
 
             printf("A circle with radius %d: \n"
                     "\t Diameter: %f \n"
                     "\t Area: %f \n"
                     "\t Circumference: %f \n",radius, diameter, area, circumference); // Print the results of the previous functions.
         }
+
+    
     return 0;
 } // End of main.
+
