ab2030a02dbac288b64433d0e8e283d4e4201814
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-09-08 09:41:53 -0400

    C Print

---
+++ C Print.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //Use Standard Streams
+
+int main(void){
+
+    //Puts prints a String to standard output.
+    puts("Hello, World!");
+
+    //This is equivalent to:
+    printf("%s\n", "Hello, World!");
+
+    //Which is also equivalent to:
+    fprintf(stdout, "%s\n", "Hello, World");
+    return 0;
+}
