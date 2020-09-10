423a89f2ecdcdc9e659d0f7c02ceeb3402429ea6
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-09-10 10:29:02 -0400

    In Class Example

---
+++ pythagoreanTheorem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int base, hypotenuse, perpendicular, bPlusP, hypSquared;
+
+int main(void){
+    printf("Enter the Base, Perpendicular and Hypotenuse of the triangle: \n");
+    scanf("%d %d %d", &base, &perpendicular, &hypotenuse);
+
+    bPlusP = (base * base) + (perpendicular * perpendicular);
+    hypSquared = (hypotenuse * hypotenuse);
+
+    if(hypSquared == bPlusP){
+        printf("It is a right triangle. \n");
+    }else if(hypSquared > bPlusP){
+        printf("The triangle is Obtuse.");
+    }else{
+        printf("The triangle is Acute.");
+    }
+}
+
+ 
