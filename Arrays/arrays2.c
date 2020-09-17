6557ede91ba327283a6c9882886a9bcb756c46ba
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-09-17 10:44:11 -0400

    Yes

---
+++ Arrays/arrays2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stddef.h>
+
+int main(){
+    int c[5]; //Initializing array.
+    for(size_t i = 0; i < 5; i++){
+        c[i] = 2;
+    }
+    printf("%s%13s\n", "Element", "value");
+
+    for(size_t i = 0; i < 5; i++){
+        printf("%7u%13d\n", i, c[i]);
+    }
+    return 0;
+}
