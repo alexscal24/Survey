60d17c50a4ca776af3f0c9ea20be59c0f587e2ac
alexscal24 <alexscalamandre@mail.adelphi.edu>
2020-11-15 15:51:45 -0500

    Scheme Exercises

--- Scheme/Exercises.scm
+++ Scheme/Exercises.scm
@@ -21,6 +21,62 @@
 (display "\n")
 
 ;b
-(display(list 'this '(is silly)))
+(display(list 'this '(is silly))) ;Answer: (this (is silly))
+(display "\n")
 
+;c
+(display(cons 'is '(this silly?))) ;Answer: (is this silly?)
+(display "\n")
+
+;d
+(display(quote (+ 2 3))) ;Answer: (+ 2 3)
+(display "\n")
+
+;e
+(display(cons '+ '(2 3))) ;Answer: (+ 2 3)
+(display "\n")
+
+;f
+(display(car '(+ 2 3))) ;Answer: +
+(display "\n")
+
+;g
+(display(cdr '(+ 2 3))) ;Answer: (2 3)
+(display "\n")
+
+;h
+;(display(cons)) ;Answer: Error
+;(display "\n")
+
+;i
+(display(quote cons)) ;Answer: cons
+(display "\n")
+
+;j
+(display(quote (quote cons))) ;Answer: 'cons
+(display "\n")
+
+;k
+(display(car(quote (quote cons)))) ;Answer: quote
+(display "\n")
+
+;l
+(display(+ 2 3)) ;Answer: 5
+(display "\n")
+
+;m
+(display(+ '2 '3)) ;Answer: 5
+(display "\n")
+
+;n
+(display(+ (car '(2 3)) (car(cdr '(2 3))))) ;Answer: 5
+(display "\n")
+
+;o
+(display((car (list + - * /)) 2 3)) ;Answer: 5
+(display "\n")
+
+;Exercise 2.2.4
+
+(cdr '(a b)) (car(cdr '(c d)))
 (exit)
